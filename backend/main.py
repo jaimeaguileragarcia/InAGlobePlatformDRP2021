dc3651eec4c8d1aec507402841a0e2eed115eb30
Kacprzak, Izabella <izabella.kacprzak19@imperial.ac.uk>
2021-06-03 09:53:46 +0100

    Added requirements, deleted Dockerfile, modified Procfile to run backend app [IK]

--- backend/main.py
+++ backend/main.py
@@ -6,4 +6,5 @@ app = Flask(__name__)
 def my_index():
     return render_template("index.html", token="DRP_token")
 
-app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True, host='127.0.0.1', port=8080)
