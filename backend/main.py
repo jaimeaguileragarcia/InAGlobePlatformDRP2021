d9c71a4e01b3c558720627ea3b49e1851cf8fb6a
Kacprzak, Izabella <izabella.kacprzak19@imperial.ac.uk>
2021-06-03 10:16:25 +0100

    Added dummy change to change buildpack [IK]

--- backend/main.py
+++ backend/main.py
@@ -6,5 +6,6 @@ app = Flask(__name__)
 def my_index():
     return render_template("index.html", token="DRP_token")
 
+
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1', port=8080)
