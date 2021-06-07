6c36022c9e7f8856ce703bcf74e2332973ea27a2
ras19 <ras19@ic.ac.uk>
2021-06-07 16:34:26 +0100

    Reflected updated in project in project blueprint [IK, RS]

--- frontend/src/CreateProject.js
+++ frontend/src/CreateProject.js
@@ -5,6 +5,9 @@ const CreateProject = () => {
     const [name, setName] = useState("Input project name");
     const [desc, setDesc] = useState("Add some awesome description!");
     const [status, setStatus] = useState("Available");
+    const [tag, setTag] = useState("Other");
+    const [location, setLocation] = useState("");
+    const [drive_link, setDriveLink] = useState("Copy the link to the Google Drive folder for this project");
 
     const history = useHistory();
 
@@ -41,6 +44,24 @@ const CreateProject = () => {
                     <option value="Available">Available</option>
                     <option value="Completed">Completed</option>
                 </select>
+                <label>Location</label>
+                <input
+                    type="text"
+                    required
+                    value={location}
+                    onChange={(e) => setLocation(e.target.value)}
+                />
+                <label>Type of project</label>
+                <select value={tag} onChange={(e) => setTag(e.target.value)}>
+                    <option value="Social">Social</option>
+                    <option value="Education">Education</option>
+                    <option value="Wash">Wash</option>
+                    <option value="Environment">Environment</option>
+                    <option value="Health">Health</option>
+                    <option value="Other">Other</option>
+                </select>
+                <label>Google Drive folder</label>
+                <textarea required value={drive_link} onChange={(e) => setDriveLink(e.target.value)}></textarea>
                 <button>Add project</button>
             </form>
         </div>
