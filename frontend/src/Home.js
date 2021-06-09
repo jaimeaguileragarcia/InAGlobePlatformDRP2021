1dcc661b5411e5fb16f3e567b7167b5eeab9bf88
Kacprzak, Izabella <izabella.kacprzak19@imperial.ac.uk>
2021-06-09 17:17:17 +0100

    Deleted flask upgrade from Procfile [IK]

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -6,29 +6,6 @@ const Home = () => {
 
     const {data: projects, error1} = useFetch("/projects")
     const {data: todos, error2} = useFetch("/todos")
-    // const todos = [
-    //     {
-    //         id: 1,
-    //         todo_desc: "Call Jaime about the DRP project with the Department of Computing",
-    //         priority: 4,
-    //         due_date: "11th June 2021",
-    //         username: "john34"
-    //     },
-    //     {
-    //         id: 2,
-    //         todo_desc: "Read over final brief for Nicholas Wu regarding EcoWarriors",
-    //         priority: 3,
-    //         due_date: "13th June 2021",
-    //         username: "john34"
-    //     },
-    //     {
-    //         id: 3,
-    //         todo_desc: "Send the notes for the meeting on Monday to rest of the team",
-    //         priority: 1,
-    //         due_date: "16th June 2021",
-    //         username: "ethanw"
-    //     }
-    // ]
 
     return (
         <div className="home">
