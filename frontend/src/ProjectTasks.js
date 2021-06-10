6b90ab2de8ac0a5d332bccd5af324e4c01e729ba
ras19 <ras19@ic.ac.uk>
2021-06-10 07:36:57 +0100

    Created spacing for add task button in project task list and made project task list title more intuitive [RS]

--- frontend/src/ProjectTasks.js
+++ frontend/src/ProjectTasks.js
@@ -4,20 +4,22 @@ import ProjectTaskList from './ProjectTaskList'
 import useFetch from './useFetch'
 import { Link } from "react-router-dom"
 
-const ProjectTasks = () => {    
+const ProjectTasks = () => {
     const { id } = useParams();
-    const {data: project, error} = useFetch("/projects/" + id)
+    const { data: project, error } = useFetch("/projects/" + id)
 
-    const {data: project_tasks, error2} = useFetch(`/projects/${project.id}/tasks`);
+    const { data: project_tasks, error2 } = useFetch(`/projects/${project.id}/tasks`);
 
     return (
         <div className="project-tasks">
-            {project_tasks && <h1>Tasks for this project</h1> }
-            {project_tasks && <ProjectTaskList project_tasks = {project_tasks} title="Project Tasks"/> }
+            {project_tasks && <h1>{project.name}: tasks</h1>}
+            {project_tasks && <ProjectTaskList project_tasks={project_tasks} title="Project Tasks" />}
 
-            {project_tasks && <Link to={`/projects/${project.id}/add-task`} className="add-task-button">Add Task</Link> }
+            <div className="add-task-button-wrapper">
+                {project_tasks && <Link to={`/projects/${project.id}/add-task`} className="add-task-button">Add Task</Link>}
+            </div>
         </div>
     );
 }
- 
+
 export default ProjectTasks;
