9698ebad4adb708497e739e7e1ae9ecba052d386
ras19 <ras19@ic.ac.uk>
2021-06-10 13:04:37 +0100

    Changed layout of personal todos [RS]

--- frontend/src/PersonalToDo.js
+++ frontend/src/PersonalToDo.js
@@ -11,7 +11,7 @@ const PersonalToDo = ({ todos, title }) => {
 
     const [todo_desc, setTodoDesc] = useState("");
     const [priority, setPriority] = useState(1);
-    const [due_date, setDueDate] = useState(Date.now)
+    const [due_date, setDueDate] = React.useState<Date>(Date.now());
     const [username, setUsername] = useState("none");
 
     const handleSubmit = e => {
@@ -41,15 +41,16 @@ const PersonalToDo = ({ todos, title }) => {
         <div className="todo-list">
             <h2>{title}</h2>
 
-            {todos.map((todo) => (
-                <div className="todo-preview" key={todo.id}>
-                    <h2>{todo.todo_desc}</h2>
-                    <h3><strong>Priority level:</strong> {todo.priority}</h3>
-                    <h3><strong>To be done by:</strong> {todo.due_date}</h3>
-                    <button onClick={handleRemove(todo.id)}>Complete</button>
-                </div>
-            ))}
-
+            <div className="personal-todos">
+                {todos.map((todo) => (
+                    <div className="todo-preview" key={todo.id}>
+                        <h2>{todo.todo_desc}</h2>
+                        <h3><strong>Priority level:</strong> {todo.priority}</h3>
+                        <h3><strong>To be done by:</strong> {todo.due_date}</h3>
+                        <button onClick={handleRemove(todo.id)}>Complete</button>
+                    </div>
+                ))}
+            </div>
 
             <div className="add-todo-form">
                 <form onSubmit={handleSubmit}>
