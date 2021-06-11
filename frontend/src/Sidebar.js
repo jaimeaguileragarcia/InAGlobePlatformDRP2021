290744b36072b5587f89233d48ac19e9aeddd20e
Kacprzak, Izabella <izabella.kacprzak19@imperial.ac.uk>
2021-06-11 10:28:20 +0100

    Added Go to Slack button in sidebar [IK]

--- frontend/src/Sidebar.js
+++ frontend/src/Sidebar.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { slide as Menu } from 'react-burger-menu';
 import { Link } from 'react-router-dom'
+
+const slackLink = "https://slack.com/intl/en-gb/"
+
 export default props => {
   return (
     <Menu>
       <div className="menu-item" > 
       <Link to={`/`}>Home</Link>
+      </div>
+      <div className="menu-item" > 
+      <Link to={`/users/john34`}>Profile</Link>
       </div>
       <div className="menu-item" > 
       <Link to={`/users`}>All Users</Link>
@@ -14,7 +20,7 @@ export default props => {
       <Link to={`/add-user`}>Add User</Link>
       </div>
       <div className="menu-item" > 
-      <Link to={`/users/john34`}>Profile</Link>
+      <a href={slackLink} target="_blank">Go To Slack</a>
       </div>
     </Menu>
   );
