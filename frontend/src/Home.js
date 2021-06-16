c7e392a4619e115020d7720e73159c15b9dc050a
ras19 <ras19@ic.ac.uk>
2021-06-16 13:12:40 +0100

    Created personal project task previews on dashboard, hardcoded data shows that it works [RS]

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,30 +1,63 @@
-import ProjectList from './ProjectList'
-import PersonalToDo from './PersonalToDo'
-import useFetch from './useFetch'
+import ProjectList from './ProjectList';
+import MyProjectTasks from './MyProjectTasks';
+import PersonalToDo from './PersonalToDo';
+import useFetch from './useFetch';
 import useToken from './useToken';
 import { useState } from "react";
 
 const Home = () => {
 
-    const {data: todos, errorTodos, isPendingTodos} = useFetch("/todos");
-    const {setToken, token} = useToken();
+    const { data: todos, errorTodos, isPendingTodos } = useFetch("/todos");
+    const { setToken, token } = useToken();
 
-    const {data: projects, errorRProjects, isPendingRProjects} = useFetch("/user_project/username/" + token);
+    const { data: projects, errorRProjects, isPendingRProjects } = useFetch("/user_project/username/" + token);
+    const project_tasks = [
+        {
+            "description": "This is example 1",
+            "due_date": "17/06/2021",
+            "priority": 1,
+            "completed": "Completed",
+            "project_id": 2,
+            "id": 1
+        },
+        {
+            "description": "This is example 2",
+            "due_date": "18/06/2021",
+            "priority": 1,
+            "completed": "Available",
+            "project_id": 3,
+            "id": 2
+        },
+        {
+            "description": "This is example 3",
+            "due_date": "21/06/2021",
+            "priority": 2,
+            "completed": "Available",
+            "project_id": 4,
+            "id": 3
+        }
+    ]
+
+    // const {data: project_tasks, errorProjectTasks, isPendingProjectTasks} = useFetch("/assigned_task/username/" + token)
 
     return (
         <div className="home">
             <h1>Dashboard</h1>
-            <div className="home-projects" style={{float: "left", width: "600px"}}>
-                { isPendingRProjects && <h2>Loading...</h2> }
-                { projects && <ProjectList projects={projects} title="My Projects"/> }
+            <div className="home-projects" style={{ float: "left", width: "600px" }}>
+                {isPendingRProjects && <h2>Loading...</h2>}
+                {projects && <ProjectList projects={projects} title="My Projects" />}
             </div>
             <div className="homeTodos">
-                { isPendingTodos && <h2>Loading...</h2> }
-                { todos && <PersonalToDo todos={todos.filter(todo => todo.username === token)} title="Personal ToDo items"/> }
+                {isPendingTodos && <h2>Loading...</h2>}
+                {todos && <PersonalToDo todos={todos.filter(todo => todo.username === token)} title="Personal ToDo items" />}
+            </div>
+            <div className="home-project-tasks" style={{ float: "left", width: "600px" }}>
+                {isPendingRProjects && <h2>Loading...</h2>}
+                {projects && <MyProjectTasks tasks={project_tasks} title="My tasks from all projects" />}
             </div>
         </div>
-        
+
     );
 }
- 
+
 export default Home;
---
+++ frontend/src/MyProjectTasks.js
@@ -0,0 +1,22 @@
+import useFetch from './useFetch'
+import { useState } from "react";
+
+const MyProjectTasks = ({ tasks, title }) => {
+    return (
+        <div>
+            <h2>{title}</h2>
+
+            <div className="my-project-tasks">
+                {tasks.map((task) => (
+                    <div className="task-preview" key={task.id}>
+                        <h2>{task.description}</h2>
+                        <h3><strong>Priority level:</strong> {task.priority}</h3>
+                        <h3><strong>To be done by:</strong> {task.due_date}</h3>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default MyProjectTasks;
