1f06fa9e86b85110d33876e691fdd69f71b7603e
ethan <ew718@ic.ac.uk>
2021-06-17 16:48:51 +0200

    Added a blueprint function to get all users assigned to a task [EW]

--- backend/blueprints/assigned_task.py
+++ backend/blueprints/assigned_task.py
@@ -4,6 +4,7 @@ from flask import Blueprint, request, jsonify
 from backend.database_config.database import DB
 from backend.models.task_model import AssignedTask
 from backend.models.task_model import Task
+from backend.models.user_model import User
 import json
 
 assigned_task = Blueprint('assigned_task', __name__)
@@ -36,6 +37,14 @@ def get_pending_tasks_for_user(username):
                  "due_date": task.due_date} for task in (Task.query.get(x.task_id) for x in entries) if not task.completed]
     return jsonify(task_list)
 
+
+@assigned_task.route('/assigned_task/all_users/<task_id>', methods=['GET'])
+def get_users_assigned_to_task(task_id):
+    entries = AssignedTask.query.filter_by(task_id=task_id)
+    usernames = [{"firstname": user.firstname,
+                 "surname": user.surname} 
+                 for user in (User.query.get(username) for username in (x.username for x in entries))]
+    return jsonify(usernames)
 
 
 @assigned_task.route('/assigned_task', methods=['POST'])
