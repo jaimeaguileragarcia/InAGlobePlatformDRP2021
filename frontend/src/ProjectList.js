15281ae695601066c4f614ebd0c56813df045933
ras19 <ras19@ic.ac.uk>
2021-06-17 09:48:11 +0100

    Added title to project list in dashboard [RS]

--- frontend/src/ProjectList.js
+++ frontend/src/ProjectList.js
@@ -1,9 +1,10 @@
 import { Link } from 'react-router-dom'
 
-const ProjectList = ({ projects }) => {
+const ProjectList = ({ projects, title }) => {
 
     return (
         <div className="project-list">
+            <h2>{title}</h2>
             {projects.map((project) => (
                 <div className="preview" key={project.id}>
                     <Link to={`/projects/${project.id}`}>
